3fb96905abe93ab74a32b6f517189de2e6bc36d2
Alex Vernon <alex.vernon@gmail.com>
2017-09-08 02:42:57 -0400

    converted to for loop and added increment shortcut

--- guess.py
+++ guess.py
@@ -9,13 +9,15 @@ myName = input()
 number = random.randint(1, 20)
 print('Well, ' + myName + ', I am thinking of a number between 1 and 20.')
 
-while guessesTaken < 6:
+# while guessesTaken < 6:
+for guessesTaken in range(6):
     print('Take a guess.')
 
     guess = input()
     guess = int(guess)
 
-    guessesTaken = guessesTaken + 1
+    # guessesTaken = guessesTaken + 1
+    guessesTaken += 1
 
     if guess < number:
         print('Your guess is too low.')
