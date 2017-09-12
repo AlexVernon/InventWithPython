95f382b3b1c45284a4d0fd955f73fd02ecb6a31c
Alex Vernon <alex.vernon@gmail.com>
2017-09-12 02:07:32 -0400

    created jokes.py

---
+++ jokes.py
@@ -0,0 +1,18 @@
+print('What do you get when you cross a snowman with a vampire?')
+input()
+print('Frostbite!')
+print()
+
+print('What do dentists call an astronaut\'s cavity?')
+input()
+print('A black hole!')
+print()
+
+print('Knock knock.')
+input()
+print('Who\'s there?')
+input()
+print('Interupting cow.')
+input()
+print('Interupting cow wh', end='')
+print('-MOO!')
